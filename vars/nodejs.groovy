6f95d1bac8ca4bb4a5371db954d7cf47d50fd32e
unknown <kumaradarsh921@gmail.com>
2024-01-06 22:52:55 +0530

    Git Auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,14 +1,21 @@
+def lintchecks() {
+                    sh "echo Installing JSlist"
+                    sh "npm i jslint"
+                    sh "echo starting linkchecks ..."
+                    sh "node_modules/jslint/bin/jslint.js server.js || true"
+                    sh "echo linkchecks completed"
+}
+
 def call () {
         pipeline {
             agent any 
             stages {
                 stage('Lint Checks') {
                     steps {
-                        sh "echo Installing JSlist"
-                        sh "npm i jslint"
-                        sh "echo starting linkchecks ..."
-                        sh "node_modules/jslint/bin/jslint.js server.js || true"
-                        sh "echo linkchecks completed"
+                        script{
+                            lintchecks()
+                        }
+
                     }
                 }
 
