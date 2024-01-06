335eaec770a0fb68a420224b144b14c2859d7f45
unknown <kumaradarsh921@gmail.com>
2024-01-06 22:57:33 +0530

    Git Auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,9 +1,9 @@
 def lintchecks() {
                     sh "echo Installing JSlist"
                     sh "npm i jslint"
-                    sh "echo starting linkchecks ..."
+                    sh "echo starting linkchecks for $(COMPONENT)"
                     sh "node_modules/jslint/bin/jslint.js server.js || true"
-                    sh "echo linkchecks completed"
+                    sh "echo linkchecks completed for $(COMPONENT)"
 }
 
 def call () {
