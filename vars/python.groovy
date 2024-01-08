15d929a9a82ebd6de7395c093b48df585009c337
unknown <kumaradarsh921@gmail.com>
2024-01-08 18:02:40 +0530

    Git Auto push

---
+++ vars/python.groovy
@@ -0,0 +1,29 @@
+def lintchecks() {
+                    sh "echo Installing Pylint"
+                    sh "pylint mymodule.py"
+                    sh "echo linkchecks completed for ${COMPONENT}"
+}
+
+def call () {
+        pipeline {
+            agent any 
+            stages {
+                stage('Lint Checks') {
+                    steps {
+                        script{
+                            lintchecks()
+                        }
+
+                    }
+                }
+
+                stage('Generating Artifacts') {
+                    steps {
+                        sh "echo Generating Artifacts for $COMPONENT"
+                        sh "pip install pylint"
+
+                    }
+                }
+            }
+        }
+    }
