a5bc395e5cb5056fd7c1af95389beb52ed00631a
unknown <kumaradarsh921@gmail.com>
2024-01-28 14:01:56 +0530

    Git Auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -22,7 +22,7 @@ def call () {
 
                     }
                 }
-                
+
                 stage('Sonar checks') {
                     steps {
                         sh "env"
