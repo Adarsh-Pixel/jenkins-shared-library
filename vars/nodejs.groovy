69ba4d3a3c3b36650c1d06bbcb20123906fb415f
unknown <kumaradarsh921@gmail.com>
2024-01-28 13:48:36 +0530

    Git Auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -9,6 +9,10 @@ def lintchecks() {
 def call () {
         pipeline {
             agent any 
+            environment {
+                SONAR_URL = "172.31.23.141"
+                SONAR_CRED = credentials('SONAR_CRED')
+            }
             stages {
                 stage('Lint Checks') {
                     steps {
@@ -26,6 +30,13 @@ def call () {
 
                     }
                 }
+                stage('Sonar checks') {
+                    steps {
+                        sh "env"
+                        sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}"
+                    }
+                }
+
             }
         }
     }
