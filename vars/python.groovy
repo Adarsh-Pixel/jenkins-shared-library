2f90dc6d5369a2b88cc08ff857b46fc332ca3bb1
unknown <kumaradarsh921@gmail.com>
2024-02-07 19:22:17 +0530

    Git Auto push

--- vars/python.groovy
+++ vars/python.groovy
@@ -7,6 +7,10 @@ def lintchecks() {
 def call () {
         pipeline {
             agent any 
+            environment {
+                SONAR_URL = "172.31.23.141"
+                SONAR_CRED = credentials('SONAR_CRED')
+            }
             stages {
                 stage('Lint Checks') {
                     steps {
