e7fd9e79ced5d0ee1bb612da07d0462b3fcb33cc
unknown <kumaradarsh921@gmail.com>
2024-02-07 20:30:23 +0530

    Git Auto push

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,4 +1,5 @@
 def lintChecks() {
+        sh "pylint payment.py"
         sh "echo Starting lintChecks for ${COMPONENT}"
         sh "pylint *.py || true"
         sh "echo linkchecks completed for ${COMPONENT}"
