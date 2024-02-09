350b8d5f0b77d83af220bad31314dae7fd73cd34
unknown <kumaradarsh921@gmail.com>
2024-02-09 14:43:57 +0530

    Git Auto push

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -7,6 +7,10 @@ def lintchecks() {
 def call () {
         pipeline {
             agent any 
+            environment {
+                SONAR_URL = "172.31.23.141"
+                SONAR_CRED = credentials('SONAR_CRED')
+            }
             stages {
                 stage('Lint Checks') {
                     steps {
@@ -22,6 +26,21 @@ def call () {
                         sh "echo Generating Artifacts for $COMPONENT"
                         sh "mvn clean compile"
 
+                    }
+                }
+
+                stage('Sonar checks') {
+                    steps {
+                        sh "echo starting sonar checks for ${COMPONENT}"
+                        sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}"
+                        sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
+                        sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${SONAR_URL} ${COMPONENT}"
+                        sh "echo ${COMPONENT} sonar checks are completed"
+                    }
+                }
+                stage('Generating artifacts') {
+                    steps {
+                        sh "echo Artifact generation complete"
                     }
                 }
             }
