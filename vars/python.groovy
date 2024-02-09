16ac4b6c4351ac3c066b32f914e3a7e3f8d8e885
unknown <kumaradarsh921@gmail.com>
2024-02-09 13:53:06 +0530

    Git Auto push

--- vars/python.groovy
+++ vars/python.groovy
@@ -20,20 +20,20 @@ def call () {
 
                     }
                 }
-                stage('Sonar checks') {
-                    steps {
-                        sh "echo starting sonar checks for ${COMPONENT}"
-                        sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}"
-                        sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
-                        sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${SONAR_URL} ${COMPONENT}"
-                        sh "echo ${COMPONENT} sonar checks are completed"
-                    }
-                }
-                stage{'Generating artifacts'} {
-                    steps {
-                        sh "echo Artifact generation complete"
-                    }
-                }
+                // stage('Sonar checks') {
+                //     steps {
+                //         sh "echo starting sonar checks for ${COMPONENT}"
+                //         sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}"
+                //         sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
+                //         sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${SONAR_URL} ${COMPONENT}"
+                //         sh "echo ${COMPONENT} sonar checks are completed"
+                //     }
+                // }
+                // stage{'Generating artifacts'} {
+                //     steps {
+                //         sh "echo Artifact generation complete"
+                //     }
+                // }
             }
         }
     }
