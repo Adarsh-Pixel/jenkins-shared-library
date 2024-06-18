2352fcf276e73e665ff2c40e0806ef4694a15270
Adarsh-Pixel <kumaradarsh921@gmail.com>
2024-06-18 19:11:19 +0530

    Git Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -62,7 +62,7 @@ def call () {
                     }
                     steps {
                         script {
-                        def UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true")
+                        env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true")
                         print UPLOAD_STATUS
                         }
                     }
