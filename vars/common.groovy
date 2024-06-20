2d7be4e6a84d69bd94ef394d6978b26dd4b8fb08
Adarsh-Pixel <kumaradarsh921@gmail.com>
2024-06-20 16:23:50 +0530

    Git Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -71,7 +71,7 @@ def artifacts() {
                            sh "echo Generating Artifacts...."
                            sh "mvn clean package"
                            sh "mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar"
-                           sh "zip -r ${COMPONENT}-${TAG_NAME}.zip ${COMPONENT}.jar"
+                           sh "zip -r ${COMPONENT}-${TAG_NAME}.zip pom.xml"
                         }
                         else if(env.APPTYPE == "python"){
                            sh "echo Generating Artifacts...."
