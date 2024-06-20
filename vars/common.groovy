e1a092580eabb0243da49773b28441e2bc2652c6
Adarsh-Pixel <kumaradarsh921@gmail.com>
2024-06-20 15:52:52 +0530

    Git Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -53,7 +53,8 @@ def testCases() {
     }
 }
  
-def artifact() {
+def artifacts() {
+        
         stage('checking the Artifacts Release') {
               env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true")
               print UPLOAD_STATUS
